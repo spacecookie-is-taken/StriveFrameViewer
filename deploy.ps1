6a3cd4c62759802e30c5cd415e02b8cbbb05732a
Andrew <blackledgecreates@gmail.com>
2023-12-31 00:38:40 -0800

    Reduce lag during loading screens and start up crashes

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,10 @@
 # This just copies the dll to the game files and launches the game to save me a few clicks while testing
 
-Copy-Item ".\Output\StriveFrameData\Game__Debug__Win64\StriveFrameData.dll" "C:\Program Files (x86)\Steam\steamapps\common\GUILTY GEAR STRIVE\RED\Binaries\Win64\Mods\StriveFrameViewer\dlls\main.dll"
-Copy-Item ".\Output\Output\Game__Debug__Win64\UE4SS\bin\UE4SS.dll" "C:\Program Files (x86)\Steam\steamapps\common\GUILTY GEAR STRIVE\RED\Binaries\Win64\UE4SS.dll"
+$game_dir = "C:\Program Files (x86)\Steam\steamapps\common\GUILTY GEAR STRIVE\RED\Binaries\Win64\"
+
+Copy-Item ".\Output\StriveFrameData\Game__Debug__Win64\StriveFrameData.dll" "$game_dir\Mods\StriveFrameViewer\dlls\main.dll" | out-null
+Copy-Item ".\Output\Output\Game__Debug__Win64\UE4SS\bin\UE4SS.dll" "$game_dir\UE4SS.dll" | out-null
+Copy-Item ".\prereqs\mods.txt" "$game_dir\Mods\mods.txt" | out-null
+Copy-Item ".\prereqs\UE4SS-settings.ini" "$game_dir\UE4SS-settings.ini" | out-null
 
 & "C:\Program Files (x86)\Steam\steamapps\common\GUILTY GEAR STRIVE\GGST.exe"
